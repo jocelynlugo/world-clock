4b87f207cd86de891c95abcf668276f5c693d5ee
jocelynlugo <jocelynblugo@gmail.com>
2023-11-27 13:32:25 -0500

    add link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -49,7 +49,8 @@ function updateCity(event) {
           <div class="time">${cityTime.format(
             "h:mm:ss"
           )}<small>${cityTime.format("A")}</small></div>
-        </div>`;
+        </div>
+        <a href="/">All Cities</a>`;
 }
 
 let citiesSelectElement = document.querySelector("#city");
